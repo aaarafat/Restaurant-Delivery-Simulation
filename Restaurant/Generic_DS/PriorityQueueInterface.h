e72ed946f8f795767dbee56b2aa9e338596ec133
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-03-15 04:50:48 +0200

    Priority Queue Interface

---
+++ Restaurant/Generic_DS/PriorityQueueInterface.h
@@ -0,0 +1,20 @@
+#ifndef  _PRIORITY_QUEUE_INTERFACE
+#define _PRIORITY_QUEUE_INTERFACE
+
+template<class ItemType>
+class PriorityQueueInterface
+{
+public:
+	// sees whether the queue is empty
+	virtual bool isEmpty() const = 0;
+
+	// Adds new item to the queue
+	virtual bool add(const ItemType& Item) = 0;
+
+	// Remove the item with the highest priority
+	virtual bool remove() = 0;
+
+	// Returns the Item with the highest priority
+	virtual ItemType peek() const = 0;
+};
+#endif _PRIORITY_QUEUE_INTERFACE
